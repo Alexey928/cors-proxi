c4abdb7549e7d04d4bcf205538bbfc918afef9ca
Alexey928 <aleksey__tihakov@ukr.net>
2023-10-16 00:00:51 +0300

    second commit 1,2

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT =  3001;
+const cors = require('cors'); // Импорт пакета cors
 
+app.use(cors());
 app.use(express.json());
+
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
