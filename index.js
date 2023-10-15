a755334cf2bc20f7e756df97e78270dc3480d4b5
Alexey928 <aleksey__tihakov@ukr.net>
2023-10-15 14:20:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT =  3001;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.get('/proxy/:url', async (req, res) => {
+    console.log(req.params)
+    const targetUrl = decodeURIComponent(req.params.url);
+
+    try {
+        const response = await axios.get(targetUrl);
+        res.json(response.data);
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: error.message});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`CORS proxy is running on port ${PORT}`);
+});
