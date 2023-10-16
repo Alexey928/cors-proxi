6a51240d697d4ae289b4be82a5a058e35d7d9b4b
Alexey928 <aleksey__tihakov@ukr.net>
2023-10-16 09:35:02 +0300

    second commit 1,8

--- index.js
+++ index.js
@@ -2,26 +2,19 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT =  3001;
-const cors = require('cors');
 
 app.use(express.json());
-app.use(cors());
-
-let f = "ddddddd"
-
 
 app.use((req, res, next) => {
-    f="ggggggggg"
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
 app.get('/proxy/:url', async (req, res) => {
     console.log(req.params)
     const targetUrl = decodeURIComponent(req.params.url);
-    f = targetUrl
 
     try {
         const response = await axios.get(targetUrl);
@@ -32,6 +25,8 @@ app.get('/proxy/:url', async (req, res) => {
         res.status(500).json({ error: error.message});
     }
 });
+
+
 // Обработка корневого URL
 app.get('/', (req, res) => {
     res.send(f);
